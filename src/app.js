730c84a6138ff9eaf75f12baa731f3dc59fc01fb
menh <dinhthienmenh1505@gmail.com>
2025-03-21 16:05:51 +0700

    install core packages

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const app = express();
+
+//init middleware
+app.use(morgan('dev')); // combined | common | tiny | dev | short
+app.use(helmet());
+app.use(compression());
+
+//init db
+
+//init routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: 'Hello World'
+    });
+});
+
+//handling errors
+
+module.exports = app;
